55b54f8090e1ab1f4f43eac835d7bd1010aa7803
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-07-20 12:55:13 +0200

    Add definitions file for global project variables

---
+++ definitions.py
@@ -0,0 +1,8 @@
+"""
+File used to store project global variables.
+"""
+
+import os
+
+# This is the Project Root, important that definitions.py is inside this folder for accurate ROOT_DIR
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
