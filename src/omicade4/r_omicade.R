8f3859450e267eb25873ee251b47f8f33df7e931
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-08-26 12:33:45 +0200

    Import code from momix

--- src/omicade4/r_omicade.R
+++ src/omicade4/r_omicade.R
@@ -1,11 +1,24 @@
 library(omicade4)
+library("ggplot2")
 
 GE <- read.csv("/Users/bram/jointomicscomp/data/GE.csv", nrows=50)
 
 ME <- read.csv("/Users/bram/jointomicscomp/data/ME.csv", nrows=50)
 
 data <- list(GE, ME)
+num_factors <- 5
 
-mcoin <- mcia(data, cia.nf=5)
+factorizations_mcia <- mcia(data, cia.nf=num_factors)
 
-print(mcoin)
+# From Momix
+# https://github.com/ComputationalSystemsBiology/momix-notebook/blob/master/scripts/runfactorization.R
+
+factors_mcia<-as.matrix(factorizations_mcia$mcoa$SynVar)
+
+metagenes_mcia<-list()
+for(j in 1:length(data)){
+  metagenes_mcia[[j]]<-as.matrix(factorizations_mcia$mcoa$axis[1:dim(data[[j]])[1],])
+  rownames(metagenes_mcia[[j]])<-rownames(data[[j]])
+  colnames(metagenes_mcia[[j]])<-1:num_factors
+}
+
