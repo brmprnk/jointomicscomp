79ac29d2c0b4dd30fa446d43ce6cadde3eadc793
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2021-10-09 12:59:44 +0200

    Get MVIB running (first draft)

--- src/MVIB/training/__init__.py
+++ src/MVIB/training/__init__.py
@@ -1,1 +1,2 @@
 from src.MVIB.training.vae import VAETrainer
+from src.MVIB.training.omics import OmicsMIBTrainer
--- src/MVIB/utils/evaluation.py
+++ src/MVIB/utils/evaluation.py
@@ -8,12 +8,14 @@ from sklearn.preprocessing import MinMaxScaler
 class EmbeddedDataset:
 	BLOCK_SIZE = 256
 
-	def __init__(self, base_dataset, encoder, device='cpu'):
-		encoder = encoder.to(device)
-		self.means, self.target = self._embed(encoder, base_dataset, device)
+	def __init__(self, base_dataset, encoder1, encoder2, device='cpu'):
+		encoder1 = encoder1.to(device)
+		encoder2 = encoder2.to(device)
+		self.means1, self.means2, self.target = self._embed(encoder1, encoder2, base_dataset, device)
 
-	def _embed(self, encoder, dataset, device):
-		encoder.eval()
+	def _embed(self, encoder1, encoder2, dataset, device):
+		encoder1.eval()
+		encoder2.eval()
 
 		data_loader = torch.utils.data.DataLoader(
 			dataset,
@@ -21,26 +23,32 @@ class EmbeddedDataset:
 			shuffle=False)
 
 		ys = []
-		reps = []
+		reps1 = []
+		reps2 = []
 		with torch.no_grad():
-			for x, y in data_loader:
-				x = x.to(device)
-				y = y.to(device)
+			# list of omic1, omic2, y
+			for omics_data in data_loader:
+				x = omics_data[0].to(device)
+				y = omics_data[1].to(device)
+				labels = omics_data[2].to(device)
 
-				p_z_given_x = encoder(x)
+				p_z1_given_x = encoder1(x)
+				p_z2_given_y = encoder2(y)
 
-				reps.append(p_z_given_x.mean.detach())
-				ys.append(y)
+				reps1.append(p_z1_given_x.mean.detach())
+				reps2.append(p_z2_given_y.mean.detach())
+				ys.append(labels)
 
 			ys = torch.cat(ys, 0)
 
-		return reps, ys
+		return reps1, reps2, ys
 
 	def __getitem__(self, index):
 		y = self.target[index]
-		x = self.means[index // self.BLOCK_SIZE][index % self.BLOCK_SIZE]
+		x1 = self.means1[index // self.BLOCK_SIZE][index % self.BLOCK_SIZE]
+		x2 = self.means2[index // self.BLOCK_SIZE][index % self.BLOCK_SIZE]
 
-		return x, y
+		return x1, x2, y
 
 	def __len__(self):
 		return self.target.size(0)
@@ -72,27 +80,32 @@ def split(dataset, size, split_type):
 def build_matrix(dataset):
 	data_loader = torch.utils.data.DataLoader(dataset, batch_size=256, shuffle=False)
 
-	xs = []
+	x1s = []
+	x2s = []
 	ys = []
 
-	for x, y in data_loader:
-		xs.append(x)
-		ys.append(y)
+	for omics_data in data_loader:
+		x1s.append(omics_data[0])
+		x2s.append(omics_data[1])
+		ys.append(omics_data[2])
 
-	xs = torch.cat(xs, 0)
+	x1s = torch.cat(x1s, 0)
+	x2s = torch.cat(x2s, 0)
 	ys = torch.cat(ys, 0)
 
-	if xs.is_cuda:
-		xs = xs.cpu()
+	if x1s.is_cuda:
+		x1s = x1s.cpu()
+	if x2s.is_cuda:
+		x2s = x1s.cpu()
 	if ys.is_cuda:
 		ys = ys.cpu()
 
-	return xs.data.numpy(), ys.data.numpy()
+	return x1s.data.numpy(), x2s.data.numpy(), ys.data.numpy()
 
 
-def evaluate(encoder, train_on, test_on, device):
-	embedded_train = EmbeddedDataset(train_on, encoder, device=device)
-	embedded_test = EmbeddedDataset(test_on, encoder, device=device)
+def evaluate(encoder1, encoder2, train_on, test_on, device):
+	embedded_train = EmbeddedDataset(train_on, encoder1, encoder2, device=device)
+	embedded_test = EmbeddedDataset(test_on, encoder1, encoder2, device=device)
 	return train_and_evaluate_linear_model(embedded_train, embedded_test)
 
 
@@ -103,18 +116,30 @@ def train_and_evaluate_linear_model_from_matrices(x_train, y_train, solver='saga
 
 
 def train_and_evaluate_linear_model(train_set, test_set, solver='saga', multi_class='multinomial', tol=.1, C=10):
-	x_train, y_train = build_matrix(train_set)
-	x_test, y_test = build_matrix(test_set)
+	x1_train, x2_train, y_train = build_matrix(train_set)
+	x1_test, x2_test, y_test = build_matrix(test_set)
 
 	scaler = MinMaxScaler()
 
-	x_train = scaler.fit_transform(x_train)
-	x_test = scaler.transform(x_test)
+	x1_train = scaler.fit_transform(x1_train)
+	x2_train = scaler.fit_transform(x2_train)
+	x1_test = scaler.transform(x1_test)
+	x1_test = scaler.transform(x2_test)
 
 	model = LogisticRegression(solver=solver, multi_class=multi_class, tol=tol, C=C)
-	model.fit(x_train, y_train)
+	model.fit(x1_train, y_train)
 
-	test_accuracy = model.score(x_test, y_test)
-	train_accuracy = model.score(x_train, y_train)
+	test1_accuracy = model.score(x1_test, y_test)
+	train1_accuracy = model.score(x1_train, y_train)
+
+	model = LogisticRegression(solver=solver, multi_class=multi_class, tol=tol, C=C)
+	model.fit(x2_train, y_train)
+
+	test2_accuracy = model.score(x2_test, y_test)
+	train2_accuracy = model.score(x2_train, y_train)
+
+	# Now what do we do here?
+	train_accuracy = (train1_accuracy + train2_accuracy) / 2
+	test_accuracy = (test1_accuracy + test2_accuracy) / 2
 
 	return train_accuracy, test_accuracy
