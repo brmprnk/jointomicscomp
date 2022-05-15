2908aa28a204b9f54bb390790bf761d4bc155377
Bram Pronk <I.B.Pronk@student.tudelft.nl>
2022-05-15 20:21:12 +0200

    Update MOFA+ Analysis and configs

--- src/MOFA2/mofa_downstream.R
+++ src/MOFA2/mofa_downstream.R
@@ -1,14 +1,21 @@
 library(ggplot2)
 library(MOFA2)
 
-trained_model <- ("/home/bram/jointomicscomp/results/mofa_gcnme 13-09-2021 20:58:45/MOFA+/GCN_ME_trained_MOFA_model.hdf5")
-model <- load_model(trained_model, remove_inactive_factors = F)
+trained_model1 <- ("/home/bram/jointomicscomp/results/train-tcga-mofa-RNA_ADT RNAADT 03-03-2022 09:13:59/MOFA+/RNA_ADT_trained_MOFA_model.hdf5")
+trained_model2 <- ("/home/bram/jointomicscomp/results/train-tcga-mofa-RNA_ADT RNAADT 02-03-2022 21:50:31/MOFA+/RNA_ADT_trained_MOFA_model.hdf5")
+model32 <- load_model(trained_model2, remove_inactive_factors = F)
+
+model64 <- load_model(trained_model1, remove_inactive_factors = F)
 
 Z = get_expectations(model, "Z", as.data.frame = TRUE)
 W = get_expectations(model, "W", as.data.frame = TRUE)
 write.csv(Z, "/home/bram/jointomicscomp/results/mofa_gcnme 13-09-2021 20:58:45/MOFA+/Z.csv", row.names = FALSE)
 write.csv(W, "/home/bram/jointomicscomp/results/mofa_gcnme 13-09-2021 20:58:45/MOFA+/W.csv", row.names = FALSE)
 
+typeof(model)
+
+models = list(model32, model64)
+select_model(models)
 # plot_factor_cor(model)
 
 # plot_data_overview(model)
